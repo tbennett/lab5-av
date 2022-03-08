ed211db2753e7c6d2b31e03f1b2c52ad6086c665
Troy <tbennett@dvc.edu>
2022-03-08 00:54:39 -0800

    changed media files and updated names and references

---
+++ j/main.js
@@ -0,0 +1,8 @@
+
+const lowRider = document.querySelector('.low-rider');
+const leverage = document.querySelector('.leverage');
+
+lowRider.preload = true;
+lowRider.volume = 0.5;
+
+leverage.volume = 0.5;
