604b6233185e98feb79daf3350c1fb7634537f2b
Troy <tbennett@dvc.edu>
2022-03-10 11:59:46 -0800

    added test stop button

--- j/main.js
+++ j/main.js
@@ -1,8 +1,14 @@
 
 const lowRider = document.querySelector('.low-rider');
 const leverage = document.querySelector('.leverage');
+lowRider.src = "a/money.mp3";
+lowRider.load();
 
-lowRider.preload = true;
 lowRider.volume = 0.5;
 
 leverage.volume = 0.5;
+
+document.getElementById('stop').addEventListener('click', (e)=> {
+    lowRider.pause();
+    lowRider.currentTime = 0;
+});
